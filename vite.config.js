ed6cf2a6f8f34c21aedc3236274ef1c135c382f4
Olha Sladkova <sladkovaoe@gmail.com>
2023-05-16 09:00:42 +0200

    fix vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -6,33 +6,36 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({ 
-    registerType: 'autoUpdate',
-    outDir: "build",
-    manifest:{
-      name:'Генератор кіно та серіалів',
-      short_name:'КіноГенератор',
-      description:"Сайт генерації кіно або серіалів для перегляду на вечір",
-      theme_color:'#FFFFFF',
-      icons:[
-        {
-          src:'android-chrome-192x192.png',
-          sizes:'192x192',
-          type:'image/png'
-        },
-        {
-          src:'android-chrome-512x512.png',
-          sizes:'512x512',
-          type:'image/png'
-        }
-      ]
-    },
-      globDirectory:'build/',
-      globPattern:[
-          '**/*/{css,woff2,png,svg,ico,jpg,jpeg,js,json,cjs,vue,html}'
-      ],
-      swDest:'build/sw.js'
+    VitePWA({
+      devOptions: {
+        // вмикати якщо є необхідність потестувати саме PWA-функції локально
+        // коли увімкнено – додаток кешує index.html та інші файли, тому треба не забувати робити "Clear site data" в браузері за потреби
+        // після вимкнення – в браузері все ще залишиться зареєстрований SW, якому краще зробити "Unregister"
+        // enabled: true
+      },
+      manifest: {
+        name:'Генератор кіно та серіалів',
+        short_name:'КіноГенератор',
+        description:"Сайт генерації кіно або серіалів для перегляду на вечір",
+        theme_color:'#FFFFFF',
+        icons:[
+          {
+            src:'android-chrome-192x192.png',
+            sizes:'192x192',
+            type:'image/png'
+          },
+          {
+            src:'android-chrome-512x512.png',
+            sizes:'512x512',
+            type:'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPattern:[
+            '**/*/{js,css,html,woff2,png,svg,ico,jpg,jpeg}'
+        ],
+      },
     }),
-    
   ],
 })
