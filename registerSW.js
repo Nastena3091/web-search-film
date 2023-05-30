c368960b9de71e5b5b682c77f94302033311948a
Nastena3091 <nastena20043091@gmail.com>
2023-05-30 14:09:30 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/web-search-film/sw.js', { scope: '/web-search-film/' })})}
